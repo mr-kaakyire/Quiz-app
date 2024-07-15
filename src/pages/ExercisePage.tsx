4435c85aedd77b439fb318d9f2986082e5ed936d
Mr-Boahen <emmanuelnyantakyiboahen@gmail.com>
2024-07-15 20:29:18 +0000

    fix:corrected some bugs

--- src/pages/ExercisePage.tsx
+++ src/pages/ExercisePage.tsx
@@ -421,7 +421,7 @@ function ExercisePage() {
           {data && data.questions ? (
             data?.questions[questionIndex].options?.map((option: any, index: any) => (
               <Exercise submitClicked={submitClicked} isCorrectSelection={isCorrectSelection} isSelected={selectedIndex === index} onClick={() => {
-
+                setSubmitClicked(false)
                 submitClicked == false && updateIndex(index)
               }} key={index} svgBgColor="#f5f6fa">
                 {index == 0 && <div className="svg-container">
@@ -442,7 +442,7 @@ function ExercisePage() {
                   <div style={{ height: "50px", width: "50px", display: "flex", placeItems: "center", flexShrink: 0 }}>
                     {submitClicked && (correctAnswer == option) && selectedIndex == index && <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#26D782" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-1.875 15.105L25.3 15.41a1.25 1.25 0 0 1 1.915 1.593l-.145.174-8.06 8.08a1.25 1.25 0 0 1-1.595.148l-.175-.145-4.375-4.375a1.25 1.25 0 0 1 1.595-1.913l.175.143 3.49 3.49Z" /></svg>
                     }
-                    { (correctAnswer == option)&&selectedIndex!=index &&submitClicked && <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#26D782" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-1.875 15.105L25.3 15.41a1.25 1.25 0 0 1 1.915 1.593l-.145.174-8.06 8.08a1.25 1.25 0 0 1-1.595.148l-.175-.145-4.375-4.375a1.25 1.25 0 0 1 1.595-1.913l.175.143 3.49 3.49Z" /></svg>
+                    { (correctAnswer == option)&&selectedIndex!=index &&selectedIndex!=null &&submitClicked && <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#26D782" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-1.875 15.105L25.3 15.41a1.25 1.25 0 0 1 1.915 1.593l-.145.174-8.06 8.08a1.25 1.25 0 0 1-1.595.148l-.175-.145-4.375-4.375a1.25 1.25 0 0 1 1.595-1.913l.175.143 3.49 3.49Z" /></svg>
                     }
 
                         {submitClicked && (correctAnswer != option) && selectedIndex == index && <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" viewBox="0 0 40 40"><path fill="#EE5454" d="M20 5a15 15 0 1 1 0 30 15 15 0 0 1 0-30Zm0 2.5a12.5 12.5 0 1 0 0 25 12.5 12.5 0 0 0 0-25Zm-5.402 7.415.142-.175a1.25 1.25 0 0 1 1.595-.143l.175.143L20 18.233l3.49-3.493a1.25 1.25 0 0 1 1.595-.143l.175.143a1.25 1.25 0 0 1 .142 1.595l-.142.175L21.767 20l3.493 3.49a1.25 1.25 0 0 1 .142 1.595l-.142.175a1.25 1.25 0 0 1-1.595.142l-.175-.142L20 21.767l-3.49 3.493a1.25 1.25 0 0 1-1.595.142l-.175-.142a1.25 1.25 0 0 1-.143-1.595l.143-.175L18.233 20l-3.493-3.49a1.25 1.25 0 0 1-.143-1.595Z" /></svg>
@@ -478,7 +478,7 @@ function ExercisePage() {
               }} className="next-button">
                 Next Question
               </div> : <div onClick={() => {
-               
+                setSubmitClicked(true)
                 selectedIndex && handleSubmit()
               }} className="submit-button">
                 Submit Answer
